902e2e3434dc14e35c90759475ff5bc6d6cfb2c5
Josh Blad <jpb82@byu.edu>
2023-03-08 22:35:26 -0700

    Cart icon and finishing touches

---
+++ Mission9Assignment/Views/Shared/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission9Assignment.Models;
+
+namespace Mission9Assignment.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket basketService)
+        {
+            basket = basketService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
